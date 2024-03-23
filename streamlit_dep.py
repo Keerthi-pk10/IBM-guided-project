9fe84c37d82e53a2efbf59a94ce3048487e1552f
KEERTHI P <124553107+Keerthi-pk10@users.noreply.github.com>
2024-03-23 10:56:32 +0530

    Update streamlit_dep.py

--- streamlit_dep.py
+++ streamlit_dep.py
@@ -7,9 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1WyGB4O-PypR697IDNXS9xrU6M_gOZza0
 """
 
-pip install streamlit
-
-pip install Writefile
+!pip install streamlit
 
 # Commented out IPython magic to ensure Python compatibility.
 # %%writefile st.py
